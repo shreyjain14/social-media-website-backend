08aa8e7049d3edcda32af1109de6d1ad8df50c90
Shrey <shreyjain1403@gmail.com>
2024-09-18 14:14:53 +0530

    now you need to be loggedin to create thoughts

--- website/api_thoughts.py
+++ website/api_thoughts.py
@@ -2,15 +2,20 @@ from datetime import datetime
 from flask import Blueprint, request, jsonify
 from .extentions import db
 from .models import Thought
+from flask_jwt_extended import jwt_required, current_user
 
 api_thoughts = Blueprint('api_thoughts', __name__)
 
 
 @api_thoughts.route('/create', methods=['POST'])
+@jwt_required()
 def create():
     content = request.headers['content']
 
-    thought = Thought(content=content, date=datetime.now())
+    if request.headers['anonymous'] == 'true':
+        thought = Thought(content=content, date=datetime.now())
+    else:
+        thought = Thought(content=content, date=datetime.now(), user_id=current_user.username)
     db.session.add(thought)
     db.session.commit()
 
